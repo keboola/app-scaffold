8f0588d7db59f01dd4e31e5d02cf8a44dd971c1b
Martin Zajic <zajca@zajca.cz>
2019-09-06 16:12:34 +0200

    refactor component to App class, split config for components and orchestrations

---
+++ src/App.php
@@ -0,0 +1,183 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyComponent;
+
+use Keboola\Orchestrator\Client as OrchestratorClient;
+use Keboola\StorageApi\Components;
+use Keboola\StorageApi\Options\Components\Configuration;
+use Keboola\StorageApi\Options\Components\ConfigurationRow;
+use Psr\Log\LoggerInterface;
+
+class App
+{
+    /**
+     * @var array
+     * aggreage configurations ids for use in orchestrations
+     */
+    private $configurationIdStorage = [];
+
+    /**
+     * @var array
+     * Static config loaded from FS
+     */
+    private $scaffoldStatisConfiguration;
+
+    /**
+     * @var array
+     * Variables from KBC
+     */
+    private $scaffoldParameters;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var Components
+     */
+    private $componentsApiClient;
+
+    /**
+     * @var OrchestratorClient
+     */
+    private $orchestrationApiClient;
+
+    public function __construct(
+        array $scaffoldStatisConfiguration,
+        array $scaffoldParameters,
+        Components $componentsApiClient,
+        OrchestratorClient $orchestrationApiClient,
+        LoggerInterface $logger
+    ) {
+        $this->scaffoldStatisConfiguration = $scaffoldStatisConfiguration;
+        $this->scaffoldParameters = $scaffoldParameters;
+        $this->logger = $logger;
+        $this->componentsApiClient = $componentsApiClient;
+        $this->orchestrationApiClient = $orchestrationApiClient;
+    }
+
+    public function createComponentsConfigurations(): void
+    {
+        if (array_key_exists('components', $this->scaffoldStatisConfiguration)) {
+            foreach ($this->scaffoldStatisConfiguration['components'] as $component) {
+                // primary used is id key which must be unique when two same components are used
+                // otherwise sapiComponentId is used TODO: name this, maybe KBCComponentId?
+                if (array_key_exists('id', $component)) {
+                    $componentId = $this->normalizeId($component['id']);
+                } else {
+                    $componentId = $this->normalizeId($component['sapiComponentId']);
+                }
+
+                $parameters = null;
+                if (array_key_exists($componentId, $this->scaffoldParameters)) {
+                    $parameters = $this->scaffoldParameters[$componentId];
+                }
+
+                $this->createComponentConfiguration($component, $componentId, $parameters);
+            }
+        }
+    }
+
+    public function createOrchestrations(): void
+    {
+        if (array_key_exists('orchestrations', $this->scaffoldStatisConfiguration)) {
+            foreach ($this->scaffoldStatisConfiguration['orchestrations'] as $orchestration) {
+                $this->createOrchestrationConfiguration($orchestration);
+            }
+        }
+    }
+
+    private function createOrchestrationConfiguration(
+        array $orchestratorConfig
+    ): void {
+        $this->logger->info(sprintf('Creating configuration for orchstration'));
+        $orchestratorOptions = [];
+
+        if (array_key_exists('tasks', $orchestratorConfig)) {
+            $orchestratorOptions['tasks'] = [];
+            foreach ($orchestratorConfig['tasks'] as $task) {
+                $componentConfigurationId = $this->normalizeId($task['id']);
+                $orchestratorOptions['tasks'][] = array_merge(
+                    $task,
+                    [
+                        'actionParameters' => [
+                            'config' => $this->configurationIdStorage[$componentConfigurationId],
+                        ],
+                    ]
+                );
+            }
+        }
+
+        $response = $this->orchestrationApiClient->createOrchestration(
+            $orchestratorConfig['name'],
+            $orchestratorOptions
+        );
+
+        $this->logger->info(sprintf('Orchestration %s created', $response['id']));
+    }
+
+    private function createComponentConfiguration(
+        array $componentConfig,
+        string $componentId,
+        ?array $parameters
+    ): void {
+        $this->logger->info(
+            sprintf(
+                'Creating configuration for component %s with name %s',
+                $componentConfig['sapiComponentId'],
+                $componentConfig['name']
+            )
+        );
+        $componentConfiguration = new Configuration;
+        $componentConfiguration->setComponentId($componentConfig['sapiComponentId']);
+        $componentConfiguration->setName($componentConfig['name']);
+        $componentConfiguration->setChangeDescription(
+            sprintf(
+                'KBC Scaffold component %s created',
+                $componentConfig['name']
+            )
+        );
+
+        //merge static configuration with dynamic parameters
+        $configuration = [];
+        if (array_key_exists('configuration', $componentConfig)) {
+            $configuration = $componentConfig['configuration'];
+        }
+        if ($parameters !== null) {
+            $configuration = array_merge_recursive($configuration, $parameters);
+        }
+        $componentConfiguration->setConfiguration($configuration);
+
+        $response = $this->componentsApiClient->addConfiguration($componentConfiguration);
+        $componentConfiguration->setConfigurationId($response['id']);
+
+        $this->logger->info(
+            sprintf(
+                'Configuration for component %s with id %s created',
+                $componentId,
+                $response['id']
+            )
+        );
+
+        // save id for use in orchestration
+        $this->configurationIdStorage[$componentId] = $response['id'];
+
+        if (array_key_exists('rows', $componentConfig)) {
+            foreach ($componentConfig['rows'] as $row) {
+                $rowConfiguration = new ConfigurationRow($componentConfiguration);
+                $rowConfiguration->setName($row['name']);
+                $rowConfiguration->setConfiguration($row['configuration']);
+                $rowConfiguration->setChangeDescription(sprintf('KBC Scaffold row %s added', $row['name']));
+                $this->componentsApiClient->addConfigurationRow($rowConfiguration);
+            }
+        }
+    }
+
+    private function normalizeId(string $id): string
+    {
+        return str_replace('-', '_', $id);
+    }
+}
--- src/Component.php
+++ src/Component.php
@@ -6,22 +6,14 @@ namespace MyComponent;
 
 use Exception;
 use Keboola\Component\BaseComponent;
+use Keboola\Orchestrator\Client as OrchestratorClient;
 use Keboola\StorageApi\Client;
 use Keboola\StorageApi\Components;
-use Keboola\StorageApi\Options\Components\Configuration;
-use Keboola\StorageApi\Options\Components\ConfigurationRow;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\SplFileInfo;
-use Keboola\Orchestrator\Client as OrchestratorClient;
 
 class Component extends BaseComponent
 {
-    /**
-     * @var array
-     * aggreage configurations ids for use in orchestrations
-     */
-    private $configurationIdStorage = [];
-
     protected function getConfigClass(): string
     {
         return Config::class;
@@ -40,8 +32,7 @@ class Component extends BaseComponent
         //only one scaffold can be passed in parameters
         $scaffoldParameters = $config->getScaffoldParameters();
 
-        $fs = new Filesystem;
-        $scaffoldConfiguration = $this->getScaffoldConfiguration($fs, $config->getScaffoldName());
+        $scaffoldConfiguration = $this->getScaffoldConfiguration($config->getScaffoldName());
 
         $client = new Client(
             [
@@ -50,44 +41,31 @@ class Component extends BaseComponent
                 'logger' => $this->getLogger(),
             ]
         );
-        $componentApi = new Components($client);
+        $componentsApiClient = new Components($client);
 
-        foreach ($scaffoldConfiguration['components'] as $component) {
-            if ($component['type'] === 'orchestration') {
-                // create orchestration
-                $orchestratorClient = OrchestratorClient::factory([
-                    'token' => getenv('KBC_TOKEN'),
-                ]);
-                $this->createOrchestrationConfiguration($component, $orchestratorClient);
-            } else {
-                // create component
+        $orchestrationApiClient = OrchestratorClient::factory([
+            'token' => getenv('KBC_TOKEN'),
+        ]);
 
-                // primary used is id key which must be unique when two same components are used
-                // otherwise sapiComponentId is used TODO: name this, maybe KBCComponentId?
-                if (array_key_exists('id', $component)) {
-                    /** @var string $componentId */
-                    $componentId = str_replace('-', '_', $component['id']);
-                } else {
-                    /** @var string $componentId */
-                    $componentId = str_replace('-', '_', $component['sapiComponentId']);
-                }
+        $app = new App(
+            $scaffoldConfiguration,
+            $scaffoldParameters,
+            $componentsApiClient,
+            $orchestrationApiClient,
+            $this->getLogger()
+        );
 
-                $parameters = null;
-                if (array_key_exists($componentId, $scaffoldParameters)) {
-                    $parameters = $scaffoldParameters[$componentId];
-                }
-
-                $this->createComponentConfiguration($component, $componentId, $componentApi, $parameters);
-            }
-        }
+        $app->createComponentsConfigurations();
+        $app->createOrchestrations();
     }
 
     // load scaffold config.json file
-    private function getScaffoldConfiguration(Filesystem $fs, string $scaffoldName): array
+    private function getScaffoldConfiguration(string $scaffoldName): array
     {
         $scaffoldFolder = '/code/scaffolds/' . $scaffoldName;
         $scaffoldConfigFile = $scaffoldFolder . '/config.json';
 
+        $fs = new Filesystem;
         if (!$fs->exists($scaffoldFolder)) {
             throw new Exception(sprintf('Scaffold name: %s does\'t exists.', $scaffoldName));
         }
@@ -95,91 +73,5 @@ class Component extends BaseComponent
         $fileHandler = new SplFileInfo($scaffoldConfigFile, '', 'config.json');
 
         return json_decode($fileHandler->getContents(), true);
-    }
-
-    private function createComponentConfiguration(
-        array $componentConfig,
-        string $componentId,
-        Components $componentApi,
-        ?array $parameters
-    ): void {
-        $this->getLogger()->info(
-            sprintf(
-                'Creating configuration for component %s with name %s',
-                $componentConfig['sapiComponentId'],
-                $componentConfig['name']
-            )
-        );
-        $componentConfiguration = new Configuration;
-        $componentConfiguration->setComponentId($componentConfig['sapiComponentId']);
-        $componentConfiguration->setName($componentConfig['name']);
-        $componentConfiguration->setChangeDescription(
-            sprintf(
-                'KBC Scaffold component %s created',
-                $componentConfig['name']
-            )
-        );
-
-        //merge static configuration with dynamic parameters
-        $configuration = [];
-        if (array_key_exists('configuration', $componentConfig)) {
-            $configuration = $componentConfig['configuration'];
-        }
-        if ($parameters !== null) {
-            $configuration = array_merge_recursive($configuration, $parameters);
-        }
-        $componentConfiguration->setConfiguration($configuration);
-
-        $response = $componentApi->addConfiguration($componentConfiguration);
-        $componentConfiguration->setConfigurationId($response['id']);
-
-        $this->getLogger()->info(
-            sprintf(
-                'Configuration for component %s with id %s created',
-                $componentId,
-                $response['id']
-            )
-        );
-
-        // save id for use in orchestration
-        $this->configurationIdStorage[$componentId] = $response['id'];
-
-        if (array_key_exists('rows', $componentConfig)) {
-            foreach ($componentConfig['rows'] as $row) {
-                $rowConfiguration = new ConfigurationRow($componentConfiguration);
-                $rowConfiguration->setName($row['name']);
-                $rowConfiguration->setConfiguration($row['configuration']);
-                $rowConfiguration->setChangeDescription(sprintf('KBC Scaffold row %s added', $row['name']));
-                $componentApi->addConfigurationRow($rowConfiguration);
-            }
-        }
-    }
-
-    private function createOrchestrationConfiguration(
-        array $orchestratorConfig,
-        OrchestratorClient $orchestratorApi
-    ): void {
-        $this->getLogger()->info(sprintf('Creating configuration for orchstration'));
-        $orchestratorOptions = [];
-
-        if (array_key_exists('tasks', $orchestratorConfig)) {
-            $orchestratorOptions['tasks'] = [];
-            foreach ($orchestratorConfig['tasks'] as $task) {
-                /** @var string $componentConfigurationId */
-                $componentConfigurationId = str_replace('-', '_', $task['id']);
-                $orchestratorOptions['tasks'][] = array_merge(
-                    $task,
-                    [
-                        'actionParameters' => [
-                            'config' => $this->configurationIdStorage[$componentConfigurationId],
-                        ],
-                    ]
-                );
-            }
-        }
-
-        $orchestratorApi->createOrchestration($orchestratorConfig['name'], $orchestratorOptions);
-
-        $this->getLogger()->info(sprintf('Orchestration created'));
     }
 }
