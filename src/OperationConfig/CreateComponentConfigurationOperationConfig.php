9258c74e6e1efdfd30edd5db99acd19775591332
odin <odinuv@gmail.com>
2019-10-08 18:43:20 +0200

    fix: scaffold definition

--- src/OperationConfig/CreateComponentConfigurationOperationConfig.php
+++ src/OperationConfig/CreateComponentConfigurationOperationConfig.php
@@ -69,7 +69,7 @@ class CreateComponentConfigurationOperationConfig implements OperationConfigInte
             }
             $config->payload['configuration'] = array_merge_recursive(
                 $config->payload['configuration'],
-                ['parameters' => $parameters[$config->getId()]]
+                $parameters[$config->getId()]
             );
         }
 
