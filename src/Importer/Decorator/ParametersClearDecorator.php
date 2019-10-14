16399f9a850eace6bbd90081c0d7a07012411d54
Martin Zajic <zajca@zajca.cz>
2019-10-14 11:52:18 +0200

    add new ParametersClearDecorator

---
+++ src/Importer/Decorator/ParametersClearDecorator.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\ScaffoldApp\Importer\Decorator;
+
+use Keboola\ScaffoldApp\Importer\OperationImport;
+
+class ParametersClearDecorator implements DecoratorInterface
+{
+    private const CRYPTED_KEY_PREFIX = '#';
+
+    public function getDecoratedProjectImport(
+        OperationImport $operationImport
+    ): OperationImport {
+        $payload = $this->clearCryptedParametersRecursive($operationImport->getPayload());
+
+        return new OperationImport(
+            $operationImport->getOperationId(),
+            $operationImport->getComponentId(),
+            $payload,
+            $operationImport->getTask(),
+            $operationImport->getConfigurationRows()
+        );
+    }
+
+    private function clearCryptedParametersRecursive(array $payload): array
+    {
+        foreach ($payload as $key => &$value) {
+            if (is_int($key)) {
+                continue;
+            }
+
+            if (is_array($value)) {
+                $value = $this->clearCryptedParametersRecursive($value);
+            } else {
+                if ($this->isValueCrypted($key)) {
+                    // clear value
+                    $value = '';
+                }
+            }
+        }
+
+        return $payload;
+    }
+
+    private function isValueCrypted(string $key): bool
+    {
+        return self::CRYPTED_KEY_PREFIX === substr($key, 0, 1);
+    }
+
+    public function supports(OperationImport $operationImport): bool
+    {
+        $payload = $operationImport->getPayload();
+        if (!isset($payload['configuration'])) {
+            return false;
+        }
+
+        if (!isset($payload['configuration']['parameters'])) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ tests/phpunit/Importer/Decorator/ParametersClearDecoratorTest.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\ScaffoldApp\Tests\Importer\Decorator;
+
+use Keboola\ScaffoldApp\Importer\Decorator\ParametersClearDecorator;
+use Keboola\ScaffoldApp\Importer\OperationImport;
+use Keboola\ScaffoldApp\Importer\OperationImportFactory;
+use Keboola\ScaffoldApp\Tests\Importer\ImporterBaseTestCase;
+
+class ParametersClearDecoratorTest extends ImporterBaseTestCase
+{
+    public function testGetDecoratedProjectImport(): void
+    {
+        $task = $this->getExampleOrchstrationTask();
+        $task->setComponent('transformation');
+
+        $configuration = [
+            'name' => 'configurationName',
+            'configuration' => [
+                'parameters' => [
+                    '#secret' => 'CryptedValue',
+                    'notSecret' => 'preservedvalue',
+                    'array' => ['x', 'x2'],
+                    'recursive' => [
+                        '#secret' => 'CryptedValue',
+                        'notSecret' => 'preservedvalue',
+                        'array' => ['x', 'x2'],
+                        'recursive' => [
+                            '#secret' => 'CryptedValue',
+                            'notSecret' => 'preservedvalue',
+                            'array' => ['x', 'x2'],
+                        ],
+                    ],
+                ],
+                'processors' => [
+                    'after' => [],
+                ],
+            ],
+            'rows' => [],
+        ];
+
+        $operationImport = OperationImportFactory::createOperationImport(
+            $configuration,
+            $task
+        );
+
+        self::assertInstanceOf(OperationImport::class, $operationImport);
+        self::assertStringStartsWith('transformation_', $operationImport->getOperationId());
+        self::assertEquals('transformation', $operationImport->getComponentId());
+        self::assertStringStartsWith(
+            'transformation_',
+            $operationImport->getOrchestrationTaskJsonArray()['operationReferenceId']
+        );
+
+        $expectedPayload = [
+            'name' => 'configurationName',
+            'configuration' => [
+                'parameters' => [
+                    '#secret' => '',
+                    'notSecret' => 'preservedvalue',
+                    'array' => ['x', 'x2'],
+                    'recursive' => [
+                        '#secret' => '',
+                        'notSecret' => 'preservedvalue',
+                        'array' => ['x', 'x2'],
+                        'recursive' => [
+                            '#secret' => '',
+                            'notSecret' => 'preservedvalue',
+                            'array' => ['x', 'x2'],
+                        ],
+                    ],
+                ],
+                'processors' => [
+                    'after' => [
+                    ],
+                ],
+            ],
+        ];
+
+        self::assertSame($expectedPayload, $operationImport->getPayload());
+    }
+
+    public function testSupports(): void
+    {
+        $task = $this->getExampleOrchstrationTask();
+
+        $operationImport = OperationImportFactory::createOperationImport(
+            [
+                'name' => '',
+                'configuration' => [
+                    'parameters' => [],
+                ],
+                'rows' => [],
+            ],
+            $task
+        );
+
+        $decorator = new ParametersClearDecorator();
+
+        self::assertTrue($decorator->supports($operationImport));
+    }
+
+    public function testSupportsNoParameters(): void
+    {
+        $task = $this->getExampleOrchstrationTask();
+
+        $operationImport = OperationImportFactory::createOperationImport(
+            [
+                'name' => '',
+                'configuration' => [],
+                'rows' => [],
+            ],
+            $task
+        );
+
+        $decorator = new ParametersClearDecorator();
+
+        self::assertFalse($decorator->supports($operationImport));
+    }
+}
