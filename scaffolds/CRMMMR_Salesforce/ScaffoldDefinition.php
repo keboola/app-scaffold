a6365a326a4611f5f8e98000e63293919dd7e613
Martin Zajic <zajca@zajca.cz>
2019-10-16 12:58:09 +0200

    raw import

---
+++ scaffolds/CRMMMR_Salesforce/ScaffoldDefinition.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\Scaffolds\CRMMMR_Salesforce;
+
+use Keboola\ScaffoldApp\ScaffoldInputDefinitionInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class ScaffoldDefinition implements ScaffoldInputDefinitionInterface
+{
+    public function addInputsDefinition(
+        ArrayNodeDefinition $node
+    ): ArrayNodeDefinition {
+        // @formatter:off
+        /** @noinspection NullPointerExceptionInspection */
+        $node->ignoreExtraKeys(false)
+            ->end();
+        // @formatter:on
+        return $node;
+    }
+}
