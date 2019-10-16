e11e2816417ae6cfe24814a0488ec2536a3d9971
Martin Zajic <zajca@zajca.cz>
2019-10-16 13:30:36 +0200

    add working data

--- scaffolds/CRMMMR_Salesforce/ScaffoldDefinition.php
+++ scaffolds/CRMMMR_Salesforce/ScaffoldDefinition.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Keboola\Scaffolds\CRMMMR_Salesforce;
 
 use Keboola\ScaffoldApp\ScaffoldInputDefinitionInterface;
+use Keboola\Scaffolds\CommonDefinitions;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
 class ScaffoldDefinition implements ScaffoldInputDefinitionInterface
@@ -14,11 +14,49 @@ class ScaffoldDefinition implements ScaffoldInputDefinitionInterface
     public function addInputsDefinition(
         ArrayNodeDefinition $node
     ): ArrayNodeDefinition {
-        // @formatter:off
-        /** @noinspection NullPointerExceptionInspection */
-        $node->ignoreExtraKeys(false)
-            ->end();
-        // @formatter:on
+        // Add salesforce extractor
+        $this->appendSalesForceExtractor($node);
+        // Add snowflake writer parameters
+        $node->append(
+            CommonDefinitions::getSnowflakeWriterConfig(
+                new TreeBuilder('keboolaWrDbSnowflake_2c7b')
+            )
+        );
         return $node;
+    }
+
+    private function appendSalesForceExtractor(
+        ArrayNodeDefinition $scaffoldNode
+    ): void {
+        // @formatter:off
+        $treeBuilder = new TreeBuilder('htnsExSalesforce_7666');
+        /** @var ArrayNodeDefinition $extractorNode */
+        $extractorNode = $treeBuilder->getRootNode();
+        $extractorNode->isRequired();
+        $extractorNode
+            ->children()
+                ->arrayNode('parameters')
+                    ->children()
+                        ->scalarNode('loginname')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->scalarNode('#password')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->scalarNode('#securitytoken')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->booleanNode('sandbox')
+                            ->defaultValue(false)
+                        ->end()
+                    ->end()
+                ->end() // parameters
+            ->end()
+        ;
+        // @formatter:on
+        $scaffoldNode->append($extractorNode);
     }
 }
