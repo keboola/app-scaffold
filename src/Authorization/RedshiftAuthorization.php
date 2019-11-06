58bd120ede3b10a6ff7f64d27bebf8708f9eb685
odin <odinuv@gmail.com>
2019-11-06 18:52:54 +0100

    fix: remove provisioned credentials encryption, fixes #97

--- src/Authorization/RedshiftAuthorization.php
+++ src/Authorization/RedshiftAuthorization.php
@@ -39,16 +39,10 @@ class RedshiftAuthorization implements AuthorizationInterface
             'database' => $data['connection']['database'],
             'schema' => $data['connection']['schema'],
             'user' => $data['connection']['user'],
-            '#password' => $data['connection']['password'],
+            'password' => $data['connection']['password'],
             'port' => '5439',
             'driver' => 'redshift',
         ];
-        $tokenInfo = $storageClient->verifyToken();
-        $configData = $encryptionClient->encryptConfigurationData(
-            $configData,
-            $configuration->getComponentId(),
-            (string) $tokenInfo['owner']['id']
-        );
         $configuration->setConfiguration($configData);
         $components = new Components($storageClient);
         $components->updateConfiguration($configuration);
