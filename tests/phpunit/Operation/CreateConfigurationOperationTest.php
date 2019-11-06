9cf1c741f1f7839539b5440e6251c588c5e8c179
odin <odinuv@gmail.com>
2019-11-06 18:59:18 +0100

    fix: test expectation

--- tests/phpunit/Operation/CreateConfigurationOperationTest.php
+++ tests/phpunit/Operation/CreateConfigurationOperationTest.php
@@ -143,7 +143,7 @@ class CreateConfigurationOperationTest extends BaseOperationTestCase
         $executionMock = self::getExecutionContext([], $parameters);
 
         $apiClientStoreMock = self::getApiClientStore();
-        $encryptionApiClient = self::getMockEncryptionApiClient(self::exactly(2));
+        $encryptionApiClient = self::getMockEncryptionApiClient(self::exactly(1));
         $apiClientStoreMock->method('getEncryptionApiClient')->willReturn($encryptionApiClient);
         $apiClientStoreMock->method('getComponentsApiClient')->willReturn($this->componentsApiClient);
         $this->sapiClient->method('apiPost')->willReturn([
@@ -227,7 +227,7 @@ class CreateConfigurationOperationTest extends BaseOperationTestCase
         $apiClientStoreMock = self::getApiClientStore(
             $this->sapiClient,
             $this->componentsApiClient,
-            self::getMockEncryptionApiClient(self::exactly(2)),
+            self::getMockEncryptionApiClient(self::exactly(1)),
             null
         );
 
