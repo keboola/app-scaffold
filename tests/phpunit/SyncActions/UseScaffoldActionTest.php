3ced526ddaa50835883928328efa59689388b07d
Martin Zajic <zajca@zajca.cz>
2019-11-29 11:07:37 +0100

    update UseScaffoldActionTest mock

--- tests/phpunit/SyncActions/UseScaffoldActionTest.php
+++ tests/phpunit/SyncActions/UseScaffoldActionTest.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Keboola\ScaffoldApp\Tests\SyncActions;
 
+use Keboola\Orchestrator\Client as OrchestratorClient;
 use Keboola\ScaffoldApp\SyncActions\UseScaffoldAction;
 use Keboola\ScaffoldApp\SyncActions\UseScaffoldExecutionContext\ExecutionContextLoader;
 use Keboola\ScaffoldApp\Tests\Operation\BaseOperationTestCase;
+use Keboola\StorageApi\Client;
+use Keboola\StorageApi\Components;
+use PHPUnit\Framework\MockObject\MockObject;
 use Psr\Log\NullLogger;
 
 class UseScaffoldActionTest extends BaseOperationTestCase
@@ -40,7 +44,25 @@ class UseScaffoldActionTest extends BaseOperationTestCase
             ],
             __DIR__ . '/../mock/scaffolds/PassThroughTestNoDefinition'
         );
-        $action = new UseScaffoldAction($loader->getExecutionContext(), $this->getApiClientStore(), new NullLogger());
+        /** @var MockObject|Client $sapiClientMock */
+        $sapiClientMock = self::createMock(Client::class);
+        $sapiClientMock->expects(self::exactly(2))->method('verifyToken')->willReturn(['owner' => ['id' => 1]]);
+        /** @var MockObject|Components $sapiClientMock */
+        $componentsClientMock = self::createMock(Components::class);
+        $componentsClientMock->expects(self::exactly(2))->method('addConfiguration')->willReturn(['id' => 1]);
+        $componentsClientMock->expects(self::once())->method('addConfigurationRow')->willReturn(['id' => 1]);
+        /** @var MockObject|OrchestratorClient $sapiClientMock */
+        $orchestratorClientMock = self::createMock(OrchestratorClient::class);
+        $orchestratorClientMock->expects(self::once())->method('createOrchestration')->willReturn(['id' => 1]);
+
+        $clientStoreMock = $this->getApiClientStore(
+            $sapiClientMock,
+            $componentsClientMock,
+            null,
+            $orchestratorClientMock
+        );
+
+        $action = new UseScaffoldAction($loader->getExecutionContext(), $clientStoreMock, new NullLogger());
         $response = $action->run();
         foreach ($response as $finishedOperation) {
             self::assertArrayHasKey('id', $finishedOperation);
